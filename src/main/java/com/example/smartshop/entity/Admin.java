e522e4fa3ea58a6fca8ac46358f72f5660b7c17c
Hichamelhmaydi <ct1594065@gmail.com>
2025-11-26 16:17:10 +0100

    feat: add Admin entity extending User with JPA annotations

---
+++ src/main/java/com/example/smartshop/entity/Admin.java
@@ -0,0 +1,16 @@
+package com.example.smartshop.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "admin")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Admin extends User {
+
+}
