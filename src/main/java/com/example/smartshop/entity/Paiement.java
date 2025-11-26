db644684204e9f09fdd4c5491e033a5d0398d6f0
Hichamelhmaydi <ct1594065@gmail.com>
2025-11-26 15:22:42 +0100

    Add Paiement entity for payment management

---
+++ src/main/java/com/example/smartshop/entity/Paiement.java
@@ -0,0 +1,57 @@
+package com.example.smartshop.entity;
+
+
+import com.example.smartshop.enums.StatutPaiement;
+import com.example.smartshop.enums.TypePaiement;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "paiements")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Paiement {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private Integer numeroPaiement;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal montant;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TypePaiement typePaiement;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatutPaiement statut;
+    
+    @Column(nullable = false)
+    private LocalDate datePaiement;
+    
+    @Column(nullable = false)
+    private LocalDate dateEncaissement;
+    
+    @Column(nullable = false)
+    private String reference;
+    
+    @Column(nullable = false)
+    private String banque;
+    
+    @Column(nullable = false)
+    private LocalDate dateCheance;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "commande_id", nullable = false)
+    private Commande commande;
+}
