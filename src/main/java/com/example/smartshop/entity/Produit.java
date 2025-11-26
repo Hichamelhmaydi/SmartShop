ed6dc5219612490808467217cdcdda78d84a6e1a
Hichamelhmaydi <ct1594065@gmail.com>
2025-11-26 15:25:25 +0100

    Add OneToMany relationship for LigneCommande in Produit entity

--- src/main/java/com/example/smartshop/entity/Produit.java
+++ src/main/java/com/example/smartshop/entity/Produit.java
@@ -1,11 +1,16 @@
 package com.example.smartshop.entity;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
 
@@ -32,4 +37,8 @@ public class Produit {
     private LocalDateTime dateCreation;
     @Column(nullable = false)
     private LocalDateTime dateModification;
+    
+    @OneToMany(mappedBy = "produit", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<LigneCommande> lignesCommande;
+
 }
