ab4b573ffbee84ce540be3e9db52d805e4e53614
Hichamelhmaydi <ct1594065@gmail.com>
2025-11-26 15:20:32 +0100

    Add Commande entity with attributes for order management

---
+++ src/main/java/com/example/smartshop/entity/Commande.java
@@ -0,0 +1,48 @@
+package com.example.smartshop.entity;
+
+import com.example.smartshop.enums.StatutCommande;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+@Entity
+@Table(name = "commandes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Commande {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private LocalDate dateCreation;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal sousTotalHt;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal montantReduction;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal montantTva;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalTtc;
+    
+    @Column(nullable = false)
+    private String codePromo;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatutCommande statut;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal montantRestant;
+}
