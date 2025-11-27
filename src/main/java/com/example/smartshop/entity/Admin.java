99a520637462ff44f2c78a90acdd3b272935c268
Hichamelhmaydi <ct1594065@gmail.com>
2025-11-27 15:18:17 +0100

    feat: remove NoArgsConstructor from Admin entity to streamline constructor usage

--- src/main/java/com/example/smartshop/entity/Admin.java
+++ src/main/java/com/example/smartshop/entity/Admin.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "admin")
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Admin extends User {
 
